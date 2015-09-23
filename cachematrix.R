39fef43b7d383bb30a92c2770979851b54e07bc7
qedbykb <kbryant01@brynmawr.edu>
2015-09-23 08:15:41 -0400

    first draft

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming assignment 2: The first of the two functions
+## below will take a numeric matrix 'm', and cache both the 
+## numeric matrix itself and its inverse. The second function
+## will return the inverse of whatever "matrix" it is passed, 
+## provided it is passed a cached "matrix" (i.e. NOT a true matrix) 
+## like the one returned by the first function.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## The 'makeCacheMatrix' function takes a matrix 'm' as an 
+## argument and returns a list with four functions: set, get, 
+## setinv, and getinv. Calling makeCacheMatrix(m)$get() will
+## return the matrix 'm', and calling 
+## makeCacheMatrix(m)$getinv() will return the inverse of 'm'.
 
+makeCacheMatrix <- function(m) {
+   minv <- solve(m)
+   set <- function(a) {
+      m <<- a
+      minv <<- solve(m)
+   }
+   get <- function() m
+   setinv <- function(inverse) minv <<- inverse
+   getinv <- function() minv
+   list(set = set, get = get, setinv = setinv, 
+        getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The 'cacheSolve' function takes a cached matrix 'x' as an
+## argument. The cached matrix 'x' is a list containing 
+## functions whose arguments are either empty, or a numeric
+## matrix 'm'. 'x' must contain the function getinv(), which
+## returns the inverse of 'm', and the function get(), which 
+## returns 'm'. The output of 'cacheSolve' is the inverse of 
+## the matrix 'm', which is computed either by finding it in 
+## the cache of 'x' or by computing it on the spot if its not
+## in the cache of 'x'. 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+   s <- x$getinv()
+   if(!is.null(s)) {
+      message("retrieving cached data")
+      return(s)
+   }
+   newmat <- x$get()
+   newmatinv <- solve(newmat)
+   newmatinv
 }
